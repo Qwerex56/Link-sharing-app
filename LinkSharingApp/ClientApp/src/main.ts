e35c8fe96f69c02c1e0a24c4f5e2e08758076908
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-08-25 19:52:17 +0200

    Added FA icons, some brands are missing

--- LinkSharingApp/ClientApp/src/main.ts
+++ LinkSharingApp/ClientApp/src/main.ts
@@ -5,10 +5,19 @@ import { createPinia } from "pinia"
 
 import App from './App.vue'
 import router from "../router"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import iconLib from '@/modules/fontawesome/iconLib'
+import brandLib from '@/modules/fontawesome/brandLib'
+
+library.add(...iconLib)
+library.add(...brandLib)
 
 const app = createApp(App)
 
 app.use(router)
 app.use(createPinia())
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
---
+++ LinkSharingApp/ClientApp/src/modules/fontawesome/brandLib.ts
@@ -0,0 +1,29 @@
+import {
+  faCodepen,
+  faFacebook,
+  faFreeCodeCamp,
+  faGithub,
+  faGitlab,
+  faHashnode,
+  faLinkedin,
+  faStackOverflow,
+  faTwitch,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+const brandLib = [
+  faCodepen,
+  faFacebook,
+  faFreeCodeCamp,
+  faGithub,
+  faGitlab,
+  faHashnode,
+  faLinkedin,
+  faStackOverflow,
+  faTwitch,
+  faTwitter,
+  faYoutube,
+];
+
+export default brandLib;

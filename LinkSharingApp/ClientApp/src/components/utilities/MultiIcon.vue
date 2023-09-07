d84d4951df23ad18d4b0e1cd0f8511ec5b975687
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-09-07 20:06:51 +0200

    Added missing brandicons

--- LinkSharingApp/ClientApp/src/components/utilities/MultiIcon.vue
+++ LinkSharingApp/ClientApp/src/components/utilities/MultiIcon.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex">
     <FontAwesomeIcon v-if="props.iconFa" :icon="props.iconFa" class="align-middle" />
+    <Icon v-else-if="props.iconIfy" :icon="props.iconIfy"/>
     <img v-else-if="props.iconImg" :src="props.iconImg" alt="icoI" class="align-middle">
     <img v-else :src="defIcon" alt="def" class="align-middle">
   </div>
@@ -8,6 +9,7 @@
 
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { Icon } from '@iconify/vue';
 
 const defIcon = 'src/assets/Icons/icon-link.svg';
 
@@ -15,6 +17,10 @@ const props = defineProps({
   iconFa: {
     type: Array<string>,
     required: false,
+  },
+  iconIfy: {
+    type: String,
+    require: false,
   },
   iconImg: {
     type: String,
